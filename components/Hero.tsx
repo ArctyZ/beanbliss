274618833bfe025acad1f8b5d988c2a03d91cafb
Haris <harisbaraja56@gmail.com>
2025-01-25 19:28:53 +0700

    add hero

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Hero() {
+  return (
+    <div className="bg-[url('../public/hero.jpg')] relative object-cover w-full h-screen">
+        <div className="absolute top-8 left-8 w-[80%]">
+            <h1 className="text-white font-extrabold text-4xl md:text-6xl lg:text-8xl">Awaken Your Senses with the Perfect <span className="text-primary">Bean</span></h1>
+            <p className="font-semibold text-muted text-xl">Discover the finest, freshly roasted coffee beans from around the world. Handpicked for flavor, crafted for your perfect cup.</p>
+        </div>
+    </div>
+  )
+}
