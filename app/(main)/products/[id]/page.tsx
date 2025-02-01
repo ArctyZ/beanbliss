4583967bbf0f99a6cdb10a51fe872f962a8ae536
Haris <harisbaraja56@gmail.com>
2025-02-01 19:28:21 +0700

    add product details

---
+++ app/(main)/products/[id]/page.tsx
@@ -0,0 +1,58 @@
+import AdditionalInfo from "@/components/AdditionalInfo"
+import { Button } from "@/components/ui/button"
+import { products } from "@/lib/testProduct"
+import Image from "next/image"
+
+export default function page() {
+  return (
+    <div>
+      {/* Breadcrump */}
+      <div>
+
+      </div>
+
+    {/* Product details */}
+    <div className="flex gap-2 items-center justify-center bg-primary/5">
+      {/* image */}
+      <div className="p-32">
+        <Image src={products[0].image} alt={products[0].name} height={300} width={300}/>
+      </div>
+      {/* details */}
+      <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-1">
+          <h1 className="font-semibold text-4xl tracking-wider">{products[0].name}</h1>
+          <p className="font-semibold text-md">${products[0].price}</p>
+        </div>
+        <div className="grid grid-cols-4 w-fit">
+          <div className="flex flex-col gap-2 border-[1px] p-4 items-center justify-center">
+            <span>Intensity</span>
+            <span className="font-semibold">{products[0].intensity}/10</span>
+          </div>
+          <div className="flex flex-col gap-2 border-[1px] p-5 items-center justify-center">
+            <span>Type</span>
+            <span className="font-semibold">{products[0].category}</span>
+          </div>
+          <div className="flex flex-col gap-2 border-[1px] p-5 items-center justify-center">
+            <span>Weight</span>
+            <span className="font-semibold">{products[0].weight}g</span>
+          </div>
+          <div className="flex flex-col gap-2 border-[1px] p-5 items-center justify-center">
+            <span>No. of Cups</span>
+            <span className="font-semibold">{products[0].cups}</span>
+          </div>
+        </div>
+        <div>
+          <p>{products[0].description}</p>
+        </div>
+        <div className="w-full">
+          <Button className="w-full">Add to Basket</Button>
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <AdditionalInfo/>
+    </div>
+    </div>
+  )
+}
---
+++ components/AdditionalInfo.tsx
@@ -0,0 +1,29 @@
+
+
+export default function AdditionalInfo() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-3 justify-items-center px-16 py-10 h-[50vh]">
+        <div className="flex flex-col gap-3 w-full">
+            <h3 className="text-primary text-2xl font-bold border-b-2 py-2">PRODUCT DETAIL</h3>
+            <ul className="list-disc">
+                <li>240g whole bean</li>
+                <li>Makes 26 cups</li>
+                <li>Comes in rescyclable packaging</li>
+            </ul>
+        </div>
+        <div className="flex flex-col gap-3">
+            <h3 className="text-primary text-2xl font-bold border-b-2 py-2">ADDITIONAL INFO</h3>
+            <ul className="list-disc">
+                <li>100% natural Arabica speciality coffee</li>
+                <li>Freshly roasted and ready to grind</li>
+                <li>We recommend whole bean is consumed within 3 months from roasting</li>
+                <li>Whole bean is delivered with a minimum of 2 months to consume</li>
+            </ul>
+        </div>
+        <div className="flex flex-col gap-3">
+            <h3 className="text-primary text-2xl font-bold border-b-2 py-2">WHY CHOOSE US</h3>
+            <p>Developed by world-renowed chef ####, BeanBliss offers a variety of <br /> award-winning speciality coffee, meticulously blended to double the flavour potential in every cup.</p>
+        </div>
+    </div>
+  )
+}
